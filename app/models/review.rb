ed57c9b5d001a037528cfb4d35cfc84831e7d9d1
spahmatthew@gmail.com <spahmatthew@gmail.com>
2014-08-16 19:07:55 -0700

    added reviews controller and updated routes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,2 @@
 class Review < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :book
-
-  validates :text, :user_id, :book_id, presence: true
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   root 'books#index'
   devise_for :users
-  resources :books
+  resources :books do 
+    resources :reviews
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
